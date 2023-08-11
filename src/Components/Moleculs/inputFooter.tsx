f0840e7e3e74fda2dec0a2c001dba19b1bad2425
erlanggaht <erlanggahidayat.md@gmail.com>
2023-08-11 14:18:34 +0700

    okeh

--- src/Components/Moleculs/inputFooter.tsx
+++ src/Components/Moleculs/inputFooter.tsx
@@ -2,7 +2,7 @@
 export default function InputFooter() {
   return (
     <form className={'flex items-center justify-end gap-2  '}>
-        <input id={'Inputemail'} placeholder={'asep@gmail.com'} className={'ml-2 py-3 px-4 bg-transparent border rounded-sm'} />
+        <input id={'Inputemail'} placeholder={'asep@gmail.com'} className={'ml-2 py-3 px-4 bg-transparent border rounded-sm '} />
         <button className={'py-3 border border-transparent px-4  bg-color-other-200 hover:bg-color-other-100'}>Submit</button>
     </form>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,77 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import dotenv from 'dotenv'
-
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 dotenv.config(); // load env vars from .env
+
+
+const DEFAULT_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 100,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 100,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 100,
+  },
+  tiff: {
+    // https://sharp.pixelplumbing.com/api-output#tiff
+    quality: 100,
+  },
+  // gif does not support lossless compression
+  // https://sharp.pixelplumbing.com/api-output#gif
+  gif: {},
+  webp: {
+    // https://sharp.pixelplumbing.com/api-output#webp
+    lossless: true,
+  },
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    lossless: true,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(),
+    ViteImageOptimizer(DEFAULT_OPTIONS),
+  ],
 
 })
 
